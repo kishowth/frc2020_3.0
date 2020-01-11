594ad1e070f4ce42352b4c44db3c64df171b37a1
FrostFireGames <tylerm@frostfiregames.com>
2020-01-11 14:49:54 -0500

    Update Pixy2Example.java
    Added example function to calculate distance to block

--- src/main/java/frc/sample/Pixy2Example.java
+++ src/main/java/frc/sample/Pixy2Example.java
@@ -19,6 +19,10 @@ public class Pixy2Example {
 
     private Pixy2 p2;
 
+    public static final float BALL_DIAMETER_M = 0.1778f;
+    public static final float CAMERA_FOV_HORIZONTAL_DEG = 60f;
+    public static final float CAMERA_FOV_VERTICAL_DEG = 40f;
+
     public Pixy2Example()
     {
         Pixy2 p2 = Pixy2.createInstance(LinkType.SPI);
@@ -35,5 +39,37 @@ public class Pixy2Example {
 
             SmartDashboard.putString("Debug Information", "Got block at " + positionX + ", " + positionY + ". Width = " + width);
         }
+    }
+
+    /**
+     * Calculates and returns the forward-horizontal distance between
+     * the camera and the center of the given block:
+     * 
+     *          |
+     *          |      _
+     *          |    /   \ <- Block (A ball in this case)
+     *          |   |  x  |     -   \
+     *          |    \ _ /      |    \
+     *          |               |     \
+     *          |               |       Returns this distance
+     *          |               |    /
+     *          |               -   /
+     *         \ / <-Camera  
+     */        
+    public float calculateDistanceToBlockInMeters(Block block)
+    {   
+        // Solve for this
+        float distanceToBlockInMeters;
+
+
+        int blockWidth = block.getWidth();
+        float halfFrameWidth = p2.getFrameWidth() / 2.0f;
+        float blockWidthAsPercentOfFrame = (float)blockWidth / halfFrameWidth;
+        
+        float frameWidthAtBlockInMeters = (1.0f / blockWidthAsPercentOfFrame) * BALL_DIAMETER_M;
+        double halfHorizontalFovInRadians = Math.toRadians(CAMERA_FOV_HORIZONTAL_DEG / 2.0f);
+        distanceToBlockInMeters = frameWidthAtBlockInMeters / (float)Math.tan(halfHorizontalFovInRadians);
+
+        return distanceToBlockInMeters;
     }
 }
